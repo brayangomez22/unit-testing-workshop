febce69c341c6af2aaaf9247ef27384a93dc0631
Brayan <gomezmancobrayanalexander@gmail.com>
2021-02-17 13:25:57 -0500

    first commit

---
+++ src/main/java/co/com/sofka/app/BasicCalculator.java
@@ -0,0 +1,28 @@
+package co.com.sofka.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long resta(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long multiplicaion(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long division(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 / number2;
+    }
+}
